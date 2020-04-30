38742ae05ce65e8dcd073b6ab353dbe70868b018
Trey Yoder <treyyoder@gmail.com>
2020-04-29 21:27:13 -0400

    Updates

---
+++ precache-manifest.94c3bbca13842ff575230fdad826ae25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21da8a148811ecd5bc853066f4836344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "494cc4a4632a98e71769",
+    "url": "/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "379525e0ed9385b6186e",
+    "url": "/static/css/main.da0e87a6.chunk.css"
+  },
+  {
+    "revision": "494cc4a4632a98e71769",
+    "url": "/static/js/2.4d973a36.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.4d973a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379525e0ed9385b6186e",
+    "url": "/static/js/main.8278c5e1.chunk.js"
+  },
+  {
+    "revision": "47d1c34dc2ba5cf5527f",
+    "url": "/static/js/runtime-main.12824ef6.js"
+  }
+]);
