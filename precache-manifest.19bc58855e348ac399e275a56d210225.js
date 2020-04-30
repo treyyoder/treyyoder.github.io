ef5f72a4e13c3f18b5a6464fc54d84731a871d89
Trey Yoder <treyyoder@gmail.com>
2020-04-29 21:35:41 -0400

    Updates

---
+++ precache-manifest.19bc58855e348ac399e275a56d210225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621255f382a86ca3008c269ac4412eda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e55471d6514c4b02677",
+    "url": "/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "ce3fa2b83c40a7d17c2a",
+    "url": "/static/css/main.da0e87a6.chunk.css"
+  },
+  {
+    "revision": "6e55471d6514c4b02677",
+    "url": "/static/js/2.720fbce7.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.720fbce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3fa2b83c40a7d17c2a",
+    "url": "/static/js/main.dac2bd5e.chunk.js"
+  },
+  {
+    "revision": "d35cf69d8e51e60c7bc0",
+    "url": "/static/js/runtime-main.36dc3139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94c3bbca13842ff575230fdad826ae25.js"
+  "/precache-manifest.19bc58855e348ac399e275a56d210225.js"
 );
 
 self.addEventListener('message', (event) => {
